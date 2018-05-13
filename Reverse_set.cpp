72a4183ff8a032f082b355333c67d5e20faed5bc
Tanveer Ahmed Nirjhor <36777480+nirjhor71@users.noreply.github.com>
2018-05-14 02:10:07 +0600

    Create Reverse_set.cpp

---
+++ Reverse_set.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<set>
+using namespace std;
+int main()
+{
+int n,x;
+set<int>myset;
+//set<int>::reverse_iterator rit;
+ std::set<int>::reverse_iterator rit;
+cin>>n;
+for(int i=0;i<n;i++)
+{
+  cin>>x;
+  myset.insert(x);
+} 
+cout<<myset.size()<<endl;
+
+ for ( rit=myset.rbegin(); rit!=myset.rend(); rit++)
+ {
+    std::cout<< *rit<<" ";
+
+ }
+ cout<<endl;
+}
