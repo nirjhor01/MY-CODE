fbd04e055a73caa7e879588b61cda28231f3352f
Tanveer Ahmed Nirjhor <36777480+nirjhor01@users.noreply.github.com>
2020-03-17 23:58:13 +0600

    Create MYLIB.cpp

---
+++ MYLIB.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace  std ;
+typedef long long  ll;
+typedef pair<ll, ll> Pll;
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define CASET int ___T; scanf("%d", &___T); for(int cs=1;cs<=___T;cs++)
+#define nr()  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ll ar[5000005],br[500005];
+vector<ll>a,b,c;
+map<ll,ll>m1,m2,m3;
+void Solve()
+{
+
+}
+
+int32_t main()
+{
+    nr();
+    ll T, n, nb=0, r;
+    CASET
+    {
+        Solve();
+    }
+}
